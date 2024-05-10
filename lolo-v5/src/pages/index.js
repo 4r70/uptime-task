d8102f1385e67d414c2c3788f01616a0cf9b17ed
TFT <artoreinik@gmail.com>
2024-05-10 11:57:04 +0300

    fetching works

--- lolo-v5/src/pages/index.js
+++ lolo-v5/src/pages/index.js
@@ -3,36 +3,42 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-import { parseString } from 'xml2js';
+import Parser from 'rss-parser';
 
 const inter = Inter({ subsets: ["latin"] });
 
-async function parseXML(xml) {
-  return new Promise((resolve, reject) => {
-    parseString(xml, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-}
 
 export async function getServerSideProps() {
-  const response = await fetch("https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss")
-  const xml = await response.text();
-  const data = await parseXML(xml);
-  // const parser = await fetch("https://uptime-mercury-api.azurewebsites.net/webparser", { method: "POST", body: content })
-  console.log(data)
-  return {
-    props: {
-      data: { data },
-    },
-  };
+  // const response = await fetch("https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss")
+  const parser = new Parser();
+  try {
+
+    const feed = await parser.parseURL('https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss');
+    const data = feed.items;
+
+    return {
+      props: {
+        data,
+      },
+    };
+
+  } catch (error) { 
+
+    return {
+      props: {
+        data: {  },
+        error: "Error fetching data",
+      },
+    };
+
+  }
 }
 
-export default function Home({ parser }) {
+export default function Home({ data, error }) {
+  // console.log(data)
+  if (error) {
+    console.log(error)
+  }
   return (
     <>
       <Head>
@@ -42,7 +48,13 @@ export default function Home({ parser }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-      {/* <p>{parser}</p> */}
+        {data.map((item, index) => (
+          <div key={index}>
+            <h1>{item.title}</h1>
+            <p>{item.contentSnippet}</p>
+            <p>{item.isoDate}</p>
+          </div>
+        ))}
       </main>
     </>
   );
