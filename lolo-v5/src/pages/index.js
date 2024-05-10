f2b2839134175a5aa7575b13634b7e1531d6760b
TFT <artoreinik@gmail.com>
2024-05-10 10:51:56 +0300

    xml2js

--- lolo-v5/src/pages/index.js
+++ lolo-v5/src/pages/index.js
@@ -2,19 +2,37 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { getServerSideProps } from "next/dist/build/templates/pages";
+
+import { parseString } from 'xml2js';
 
 const inter = Inter({ subsets: ["latin"] });
 
-getServerSideProps = async (context) => {
+async function parseXML(xml) {
+  return new Promise((resolve, reject) => {
+    parseString(xml, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+export async function getServerSideProps() {
+  const response = await fetch("https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss")
+  const xml = await response.text();
+  const data = await parseXML(xml);
+  // const parser = await fetch("https://uptime-mercury-api.azurewebsites.net/webparser", { method: "POST", body: content })
+  console.log(data)
   return {
     props: {
-      data: "Hello World",
+      data: { data },
     },
   };
 }
 
-export default function Home() {
+export default function Home({ parser }) {
   return (
     <>
       <Head>
@@ -24,7 +42,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        
+      {/* <p>{parser}</p> */}
       </main>
     </>
   );
